73facd6df3311c44ec4a5bd7ce155a1e897ef25b
chauhn.it@gmail.com <chauhn.it@gmail.com@91778a3d-a3a3-1f6e-e656-0f8c68645463>
2015-03-19 09:55:40 +0000

    post data to web api service

--- branches/session4/AngularFor.NetDev/Controllers/AccountController.cs
+++ branches/session4/AngularFor.NetDev/Controllers/AccountController.cs
@@ -2,26 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using AngularFor.NetDev.Models.Accounts;
 
 namespace AngularFor.NetDev.Controllers
 {
-    public class AccountController : Controller
+    public class AccountController : ApiController
     {
         //
-        // GET: /Account/
-
-        public ActionResult Index()
+        // GET: /Account/              
+        public HttpResponseMessage Save(StudentVm studentVm)
         {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult Save(StudentVm studentVm)
-        {
-            return new HttpStatusCodeResult(HttpStatusCode.OK);
+            return new HttpResponseMessage(HttpStatusCode.InternalServerError);
         }
 
     }
